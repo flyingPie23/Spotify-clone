9c2b8e12904635772716f8babd603e7a72a28e6a
flyingPie23 <drissfreah325@gmail.com>
2023-07-22 01:04:33 +0100

    added the songs show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,9 @@
 class SongsController < ApplicationController
+  def show
+    @song = Song.find(params[:id])
+    @user = User.find(@song.user_id)
+
+    @songs = Song.where(user_id: @user).sample(3)
+    @users = User.all.sample(3)
+  end
 end
